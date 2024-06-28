286843780d37482e6ecb9ab136ec01c541e72ab3
Ahmad Hedayatpour <ahmadhedayatpour2002@gmail.com>
2024-06-29 00:53:56 +0330

    Add option to choose whether to create a new Cloudflare account

--- autoG.js
+++ autoG.js
@@ -1,9 +1,52 @@
-async function createWarpAccount(count) {
+import fs from "fs";
+import inquirer from "inquirer";
+const prompt = inquirer.createPromptModule();
+
+const question = [
+  {
+    type: "list",
+    message: "Do you need new warp account ?",
+    name: "needWarpAccount",
+    choices: [
+      {
+        name: "NO",
+        value: false,
+      },
+      {
+        name: "YES",
+        value: true,
+      },
+    ],
+  },
+];
+
+async function createWarpAccounts(count) {
   const accounts = [];
   for (let i = 0; i < count; i++) {
     const req = await fetch("https://api.zeroteam.top/warp?format=sing-box");
     const warpAccount = await req.json();
     accounts.push(warpAccount);
   }
-  return accounts;
+  fs.writeFileSync("./assets/warpAccounts.json", JSON.stringify(accounts));
 }
+
+async function readWarpAccounts() {
+  const accounts = await fs.readFileSync("./assets/warpAccounts.json");
+  return JSON.parse(accounts);
+}
+
+async function autoG() {
+  const { needWarpAccount } = await prompt(question);
+  needWarpAccount && (await createWarpAccounts(2));
+
+  let accounts;
+  try {
+    accounts = await readWarpAccounts();
+    console.log(accounts);
+  } catch {
+    await createWarpAccounts(2);
+    accounts = await readWarpAccounts();
+    console.log(accounts);
+  }
+}
+autoG();
