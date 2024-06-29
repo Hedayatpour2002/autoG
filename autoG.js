1ea6396175b563476170350dbcf80520e5399e81
Ahmad Hedayatpour <ahmadhedayatpour2002@gmail.com>
2024-06-30 01:08:48 +0330

    create config

--- autoG.js
+++ autoG.js
@@ -36,7 +36,7 @@ async function readWarpAccounts() {
 }
 
 function generateIPList() {
-  const maxCount = 100;
+  const maxCount = 50;
   const temp = new Set();
   const ranges = [
     "162.159.192.",
@@ -63,18 +63,32 @@ function generateIPList() {
 
 async function checkIpPerformance(ipList) {
   const ipStats = [];
-  const ports = [903, 7103, 5279];
+  const ports = [903, 7103, 5279, 4198, 939, 928, 7156, 942, 859]; // :?
 
   for (let ip of ipList) {
     let res = await ping.promise.probe(ip);
     ipStats.push({
       ip: res.host,
       ping: res.avg,
-      packetLoss: res.packetLoss,
       port: ports[Math.floor(Math.random() * ports.length)],
     });
   }
+
+  ipStats.sort((a, b) => Number(a.ping) - Number(b.ping));
+
   return ipStats;
+}
+
+async function readConfigStructure() {
+  const configStructure = await fs.readFileSync(
+    "./assets/structure.json",
+    "utf8"
+  );
+  return JSON.parse(configStructure);
+}
+
+async function createConfigFile(config) {
+  await fs.writeFileSync("./assets/config.json", JSON.stringify(config));
 }
 
 async function autoG() {
@@ -91,5 +105,27 @@ async function autoG() {
 
   const ipList = generateIPList();
   const ipStats = await checkIpPerformance(ipList);
+
+  if (Number(ipStats[0].ping)) {
+    const config = await readConfigStructure();
+
+    config.outbounds[0].server = ipStats[0].ip;
+    config.outbounds[0].server_port = ipStats[0].port;
+    config.outbounds[1].server = ipStats[0].ip;
+    config.outbounds[1].server_port = ipStats[0].port;
+
+    config.outbounds[0].local_address = accounts[0].local_address;
+    config.outbounds[1].local_address = accounts[1].local_address;
+
+    config.outbounds[0].private_key = accounts[0].private_key;
+    config.outbounds[1].private_key = accounts[1].private_key;
+
+    config.outbounds[0].reserved = accounts[0].reserved;
+    config.outbounds[1].reserved = accounts[1].reserved;
+
+    createConfigFile(config);
+  } else {
+    throw new Error("Error: your internet is not stable");
+  }
 }
 autoG();
